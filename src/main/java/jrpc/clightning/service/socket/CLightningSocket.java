1a1e054f659432a13eeb0331280e8e6dcbd51cf0
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-02-10 23:57:18 +0100

    Adding editor conf
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- src/main/java/jrpc/clightning/service/socket/CLightningSocket.java
+++ src/main/java/jrpc/clightning/service/socket/CLightningSocket.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2019-2020 Vincenzo Palazzo vincenzo.palazzo@protonmail.com
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,7 +24,7 @@ import org.slf4j.LoggerFactory;
 /**
  * @author https://github.com/vincenzopalazzo
  */
-public class CLightningSocket extends UnixDomainSocketRpc{
+public class CLightningSocket extends UnixDomainSocketRpc {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CLightningSocket.class);
 
@@ -37,6 +37,4 @@ public class CLightningSocket extends UnixDomainSocketRpc{
         super(CLightningConfigurator.getInstance().getUrl().trim()); //TODO setting default configuration
         LOGGER.debug(CLightningConfigurator.getInstance().getUrl().trim());
     }
-
-
 }
