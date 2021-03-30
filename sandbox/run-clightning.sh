f01d905f18f42c332ef9d414ac37646afd22befa
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-03-30 12:22:25 +0200

    Fixed bash script in the sandbox
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- sandbox/run-clightning.sh
+++ sandbox/run-clightning.sh
@@ -27,7 +27,7 @@ for run in {1..50}; do
   #bitcoin-cli -datadir=$DIR/bitcoin_dir sendtoaddress "${address}" 50 "drinks" "room77" true true null "unset" null 1.1
   bitcoin-cli -datadir=$DIR/bitcoin_dir -named sendtoaddress address="${address_two}" amount=1 fee_rate=1 > /dev/null
   address="$(bitcoin-cli -datadir=$DIR/bitcoin_dir getnewaddress)"
-  bitcoin-cli -datadir=$DIR/bitcoin_dir generatetoaddress 6 "${address}" > /dev/null
+  bitcoin-cli -datadir=$DIR/bitcoin_dir generatetoaddress 50 "${address}" > /dev/null
 done
 
 ## achieve the confirmation channels for sure
