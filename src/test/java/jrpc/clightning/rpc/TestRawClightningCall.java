3419efd6a92b6b6371bc8221bbbacc839397b9ad
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-10-04 23:09:56 +0200

    Fixed tests
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
    
    # Title: 
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- src/test/java/jrpc/clightning/rpc/TestRawClightningCall.java
+++ src/test/java/jrpc/clightning/rpc/TestRawClightningCall.java
@@ -1,6 +1,9 @@
 package jrpc.clightning.rpc;
 
+import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
+import jrpc.clightning.model.CLightningGetInfo;
+import jrpc.wrapper.response.RPCResponseWrapper;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -9,9 +12,15 @@ public class TestRawClightningCall extends AbstractTestRPC {
   @Test
   public void testRawCallWithoutParams() {
     try {
-      var getInfo = rpc.rawCommand("getinfo");
+      var rawString = rpc.rawCommand("getinfo");
+      var getInfo =
+          (RPCResponseWrapper<CLightningGetInfo>)
+              converter.deserialization(
+                  rawString, new TypeToken<RPCResponseWrapper<CLightningGetInfo>>() {}.getType());
+      TestCase.assertNull(getInfo.getError());
       var rpcGetInfo = rpc.getInfo();
-      TestCase.assertEquals(converter.serialization(rpcGetInfo).trim(), getInfo.trim());
+      TestCase.assertEquals(
+          converter.serialization(rpcGetInfo).trim(), converter.serialization(getInfo.getResult()));
     } catch (IOException e) {
       TestCase.fail(e.getLocalizedMessage());
     }
@@ -20,9 +29,12 @@ public class TestRawClightningCall extends AbstractTestRPC {
   @Test
   public void testRawCallException() {
     try {
-      rpc.rawCommand("notexist");
-      TestCase.fail(
-          "This should fail, the notexist method it is not inside the c-lightning implementation");
+      var rawString = rpc.rawCommand("notexist");
+      var getInfo =
+          (RPCResponseWrapper<CLightningGetInfo>)
+              converter.deserialization(
+                  rawString, new TypeToken<RPCResponseWrapper<CLightningGetInfo>>() {}.getType());
+      TestCase.assertNull(getInfo.getResult());
     } catch (IOException ignored) {
     }
   }
